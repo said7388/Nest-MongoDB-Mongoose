a8e8727a748909e431ee0506c53037a0dd1bcc0c
abusaid7388 <abu.said@tetonelectronics.com>
2022-08-01 12:58:22 +0600

    Add env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
 import { ProjectsModule } from './projects/projects.module';
-import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+dotenv.config();
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://nest_said:WPxQTycXL9l9tFuo@cluster0.ha2x2.mongodb.net/luxury_living?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ha2x2.mongodb.net/luxury_living?retryWrites=true&w=majority`,
     ),
     ProductsModule,
     ProjectsModule,
